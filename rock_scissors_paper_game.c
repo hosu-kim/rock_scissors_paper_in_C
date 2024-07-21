cf907a13af2fbe20888597b9101ef0437692ad98
lake <145069282+hosu-kim@users.noreply.github.com>
2024-07-21 15:41:27 +0200

    Create rock_scissors_paper_game.c

---
+++ rock_scissors_paper_game.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    printf("Welcome to the Rock Scissors Paper game!\n");
+
+    printf("Choose one of the numbers.(Rock: 1, Scissors: 2, Paper: 3)\nType here: ");
+    return (0);
+}
